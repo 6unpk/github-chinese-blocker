8c71c85bc9aa004163c8fc99382dea7c072f62be
Junu Park <pkjoho95@gmail.com>
2022-06-03 09:17:49 +0900

    refine code

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
 setInterval(() => findChineseAndKill(), 500)
 function findChineseAndKill() {
-   document.querySelectorAll('ul > .repo-list-item').forEach((e) => {
-      if (/[\u3000\u3400-\u4DBF\u4E00-\u9FFF]+/.test(e.textContent)) {
+   const chineseRegExp = /[\u3000\u3400-\u4DBF\u4E00-\u9FFF]+/;
+   function removeItem (e) {
+      if (chineseRegExp.test(e.textContent)) {
          e.remove();
       }
+   }
+
+   const selectors = {
+      'repository': 'ul > .repo-list-item',
+      'issues_and_pull_request': 'div > .js-issue-row',
+   };
+
+   // repository
+   Object.keys(selectors).map((key) => {
+      document.querySelectorAll(selectors[key]).forEach(removeItem);
    });
 }
